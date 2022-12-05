d739d9a05a65964e0dd83b03ca326a8363226cdb
Sebastian Dehne <sebastian@dehnes.com>
2022-12-05 20:45:45 +0100

    calculate DVCC values

--- README.md
+++ README.md
@@ -14,6 +14,11 @@ but this tool is able to cope with multiple Daly BMSes, which is what I needed.
 It is meant to run as a service and will automatically re-connect the MQTT-connection and/or all
 serial port connections upon communications errors.
 
+'## Victron DVCC support
+Parameters, like max charge current max charge voltage and max discharge current are
+calculated and published.
+
+### Keeping Daly BMS awake 
 To prevent the Daly BMS from sleeping (and thus not respoding to data read requests)
 just set the sleep time to 65536 via the PC software once.
 
--- src/main/kotlin/com/dehnes/daly_bms_service/victron/VirtualBatteryService.kt
+++ src/main/kotlin/com/dehnes/daly_bms_service/victron/VirtualBatteryService.kt
@@ -1,12 +1,15 @@
 package com.dehnes.daly_bms_service.victron
 
-import com.dehnes.daly_bms_service.bms.*
+import com.dehnes.daly_bms_service.bms.BmsData
+import com.dehnes.daly_bms_service.bms.BmsId
+import com.dehnes.daly_bms_service.bms.BmsService
+import com.dehnes.daly_bms_service.bms.numberOfCells
 import com.dehnes.daly_bms_service.utils.PersistenceService
 import com.dehnes.daly_bms_service.utils.round2d
 import com.dehnes.daly_bms_service.victron.ValueTypes.*
 import mu.KotlinLogging
+import java.time.Instant
 import java.util.concurrent.locks.ReentrantLock
-import kotlin.math.roundToLong
 
 /**
  * Listens for data on all Daly BMses and aggregates the data only
@@ -50,32 +53,39 @@ class VirtualBatteryService(
     }
 
     private fun republishTotal() {
-        val numberOfCells = persistenceService.numberOfCells()
-        val numberOfPacks = persistenceService.numberOfPacks()
-
-
-
-        if (knownBmsData.size < numberOfPacks) {
-            logger.info { "Not sending data to Victron - have only knownBmsData.size=${knownBmsData.size}" }
+        if (knownBmsData.isEmpty()) {
+            logger.info { "Cannot send data to Victron - no BMS-data found" }
             return
         }
 
-        val minBatteryVoltage = knownBmsData.minOf { it.value.minCellVoltage } * persistenceService.numberOfCells()
-        val maxBatteryVoltage = knownBmsData.minOf { it.value.maxCellVoltage } * persistenceService.numberOfCells()
+        val numberOfCells = persistenceService.numberOfCells()
 
-        val capacityRemainig = knownBmsData.map { it.value.remainingCapacity }.sum()
-        val installedCapacity = knownBmsData.map { it.key.capacity }.sum()
-        val soc = knownBmsData.map { it.value.soc }.average().round2d()
-        val voltage = knownBmsData.map { it.value.voltage }.average().round2d()
-        val current = knownBmsData.map { it.value.current }.sum()
+        val bmsOfflineAfterSeconds = persistenceService["daly_bms.bmsOfflineAfterSeconds", "60"]!!.toLong()
+        val now = Instant.now()
+
+        val (onlineBmses, offlineBmses) = knownBmsData.values.partition {
+            it.timestamp.isAfter(now.minusSeconds(bmsOfflineAfterSeconds))
+        }
+
+        val bmsOfflineAlarm = if (offlineBmses.isNotEmpty()) 2 else 0
+
+        val minBatteryVoltage = onlineBmses.minOf { it.minCellVoltage } * persistenceService.numberOfCells()
+
+        val capacityRemainig = onlineBmses.map { it.remainingCapacity }.sum()
+        val installedCapacity = onlineBmses.map { it.bmsId.capacity }.sum()
+        val soc = onlineBmses.map { it.soc }.average().round2d()
+        val voltage = onlineBmses.map { it.voltage }.average().round2d()
+        val current = onlineBmses.sumOf { it.current }.round2d()
         val power = (voltage * current).round2d()
-        val maxTemperature = knownBmsData.map { it.value.maxTemp }.max()
-        val minTemperature = knownBmsData.map { it.value.minTemp }.min()
-        val maxCellVoltage = knownBmsData.maxBy { it.value.maxCellVoltage }.value
-        val minCellVoltage = knownBmsData.minBy { it.value.minCellVoltage }.value
-        val chargeCycles = knownBmsData.maxBy { it.value.cycles }.value.cycles
+        val maxTemperature = onlineBmses.map { it.maxTemp }.max()
+        val minTemperature = onlineBmses.map { it.minTemp }.min()
+        val maxCellVoltage = onlineBmses.maxBy { it.maxCellVoltage }
+        val minCellVoltage = onlineBmses.minBy { it.minCellVoltage }
+        val chargeCycles = onlineBmses.maxBy { it.cycles }.cycles
 
         val cellDelta = maxCellVoltage.maxCellVoltage - minCellVoltage.minCellVoltage
+
+        val controlParams: ControlParams = calculateControllParameters(onlineBmses)
 
         val toBeSendt = DbusService(
             "daly_bms_battery_1",
@@ -88,21 +98,49 @@ class VirtualBatteryService(
 
                 DbusData("/ProductId", "0", integer),
                 DbusData("/ProductName", "Daly Bms service"),
-                DbusData("/FirmwareVersion", "1.0"),
-                DbusData("/HardwareVersion", "1.0"),
+                DbusData("/FirmwareVersion", "1.0"), // TODO read from daly
+                DbusData("/HardwareVersion", "1.0"), // TODO read from daly
                 DbusData("/Connected", "1", integer),
                 DbusData("/CustomName", "Daly Bms service", writeable = true),
 
-
                 DbusData("/Info/BatteryLowVoltage", minBatteryVoltage.toString(), float),
-                DbusData("/Info/MaxChargeVoltage", maxBatteryVoltage.toString(), float),
-                DbusData("/Info/MaxChargeCurrent", 210.0.toString(), float),
-                DbusData("/Info/MaxDischargeCurrent", 350.0.toString(), float),
+
+                // DVCC data:
+                DbusData(
+                    "/Info/MaxChargeVoltage",
+                    controlParams.chargeParams.maxChargeVoltage.round2d().toString(),
+                    float
+                ),
+                DbusData(
+                    "/Info/MaxChargeCurrent",
+                    controlParams.chargeParams.maxChargeCurrent.round2d().toString(),
+                    float
+                ),
+                DbusData(
+                    "/Info/MaxDischargeCurrent",
+                    controlParams.dischargeParams.maxDischargeCurrent.round2d().toString(),
+                    float
+                ),
+                DbusData("/Io/AllowToCharge", if (controlParams.chargeParams.allowToCharge) "1" else "0", integer),
+                DbusData(
+                    "/Io/AllowToDischarge",
+                    if (controlParams.dischargeParams.allowToDischarge) "1" else "0",
+                    integer
+                ),
+
                 DbusData("/System/NrOfCellsPerBattery", numberOfCells.toString(), integer),
-                DbusData("/System/NrOfModulesOnline", "1", integer), // TODO
-                DbusData("/System/NrOfModulesOffline", "0", integer), // TODO
-                DbusData("/System/NrOfModulesBlockingCharge", "", none), // TODO
-                DbusData("/System/NrOfModulesBlockingDischarge", "", none), // TODO
+                DbusData("/System/NrOfModulesOnline", (onlineBmses.size).toString(), integer),
+                DbusData("/System/NrOfModulesOffline", (offlineBmses.size).toString(), integer),
+                DbusData(
+                    "/System/NrOfModulesBlockingCharge",
+                    controlParams.chargeParams.nrOfModulesBlockingCharge.toString(),
+                    integer
+                ),
+                DbusData(
+                    "/System/NrOfModulesBlockingDischarge",
+                    controlParams.dischargeParams.nrOfModulesBlockingDischarge.toString(),
+                    integer
+                ),
 
                 DbusData("/Capacity", capacityRemainig.toString(), float),
                 DbusData("/InstalledCapacity", installedCapacity.toString(), integer),
@@ -124,8 +162,6 @@ class VirtualBatteryService(
                 DbusData("/History/ChargeCycles", chargeCycles.toString(), integer),
                 DbusData("/History/TotalAhDrawn", "0", float), // ???
                 DbusData("/Balancing", "0", integer),
-                DbusData("/Io/AllowToCharge", "1", integer), // TODO
-                DbusData("/Io/AllowToDischarge", "1", integer), // TODO
 
 
                 DbusData("/Alarms/LowVoltage", (if (voltage < 44.8) 2 else 0).toString(), integer),
@@ -149,15 +185,132 @@ class VirtualBatteryService(
                 DbusData("/Alarms/LowChargeTemperature", (if (minTemperature < 10) 2 else 0).toString(), integer),
                 DbusData("/Alarms/HighTemperature", (if (maxTemperature > 40) 2 else 0).toString(), integer),
                 DbusData("/Alarms/LowTemperature", (if (minTemperature < 10) 2 else 0).toString(), integer),
+
+                DbusData("/Alarms/Alarm", bmsOfflineAlarm.toString(), integer),
+                DbusData("/Alarms/InternalFailure", bmsOfflineAlarm.toString(), integer),
             )
         )
-
 
         victronMqttClient.send(topic, toBeSendt)
     }
 
+    private fun calculateControllParameters(onlineBmses: List<BmsData>) =
+        ControlParams(
+            chargeParams = calculateChargeParams(onlineBmses),
+            dischargeParams = calculateDischargeParams(onlineBmses),
+        )
 
+    private fun calculateChargeParams(onlineBmses: List<BmsData>): ChargeParams {
+        val numberOfCells = persistenceService.numberOfCells()
+        val maxChargeCurrent = persistenceService["daly_bms.maxChargeCurrent", "210"]!!.toDouble()
+        val maxChargeTemp = persistenceService["daly_bms.maxChargeTemp", "45"]!!.toInt()
+        val minChargeTemp = persistenceService["daly_bms.minChargeTemp", "5"]!!.toInt()
+        val maxCellVoltage = persistenceService["daly_bms.maxCellVoltage", "3.45"]!!.toDouble()
+
+        val bmsesAllowingCharging = onlineBmses.filter { bms ->
+            val tempOK = bms.maxTemp in (minChargeTemp..maxChargeTemp)
+            val chargingOK = bms.mosfetCharging
+            val cellVoltageOK = bms.maxCellVoltage <= maxCellVoltage
+
+            if (tempOK && chargingOK && cellVoltageOK) {
+                true
+            } else {
+                logger.warn { "${bms.bmsId} does not allow charging: tempOK=$tempOK chargingOK=$chargingOK cellVoltageOK=$cellVoltageOK" }
+                false
+            }
+        }
+        val nrOfModulesBlockingCharge = onlineBmses.size - bmsesAllowingCharging.size
+
+        return if (nrOfModulesBlockingCharge > 0) {
+            ChargeParams(
+                maxChargeVoltage = (maxCellVoltage * numberOfCells),
+                maxChargeCurrent = 0.0,
+                allowToCharge = false,
+                nrOfModulesBlockingCharge
+            )
+        } else {
+            val soc = onlineBmses.map { it.soc }.average()
+
+            val chargeCurrent = if (soc >= 90) {
+                val slope = ((maxChargeCurrent - (maxChargeCurrent * 0.3)) / (100 - 90)) * -1
+                (90 - soc) * slope + maxChargeCurrent
+            } else {
+                maxChargeCurrent
+            }
+
+            ChargeParams(
+                maxChargeVoltage = (maxCellVoltage * numberOfCells),
+                maxChargeCurrent = chargeCurrent,
+                allowToCharge = true,
+                0
+            )
+        }
+
+    }
+
+    private fun calculateDischargeParams(onlineBmses: List<BmsData>): DischargeParams {
+        val maxDischargeCurrent = persistenceService["daly_bms.maxDischargeCurrent", "380"]!!.toDouble()
+        val minCellVoltage = persistenceService["daly_bms.minCellVoltage", "3.00"]!!.toDouble()
+        val minDischargeTemp = persistenceService["daly_bms.minDischargeTemp", "-10"]!!.toInt()
+        val maxDischargeTemp = persistenceService["daly_bms.maxDischargeTemp", "45"]!!.toInt()
+        val minDischargeSoC = persistenceService["daly_bms.minDischargeSoC", "10"]!!.toInt()
+
+        val soc = onlineBmses.map { it.soc }.average()
+
+        val bmsesAllowingDischarging = onlineBmses.filter { bms ->
+            val tempOK = bms.maxTemp in (minDischargeTemp..maxDischargeTemp)
+            val dischargingOK = bms.mosfetDischarging
+            val cellVoltageOK = bms.minCellVoltage >= minCellVoltage
+            val socOK = soc > minDischargeSoC
+
+            if (tempOK && dischargingOK && cellVoltageOK && socOK) {
+                true
+            } else {
+                logger.warn {
+                    "${bms.bmsId} does not allow discharging: " +
+                            "tempOK=$tempOK " +
+                            "dischargingOK=$dischargingOK " +
+                            "cellVoltageOK=$cellVoltageOK " +
+                            "socOK=$socOK"
+                }
+                false
+            }
+        }
+        val nrOfModulesBlockingDischarge = onlineBmses.size - bmsesAllowingDischarging.size
+
+        return if (nrOfModulesBlockingDischarge > 0) {
+            DischargeParams(
+                maxDischargeCurrent = 0.0,
+                allowToDischarge = false,
+                nrOfModulesBlockingDischarge
+            )
+        } else {
+            DischargeParams(
+                maxDischargeCurrent = maxDischargeCurrent,
+                allowToDischarge = true,
+                0
+            )
+        }
+    }
 }
+
+data class ChargeParams(
+    val maxChargeVoltage: Double,
+    val maxChargeCurrent: Double = 210.0,
+    val allowToCharge: Boolean,
+    val nrOfModulesBlockingCharge: Int,
+)
+
+data class DischargeParams(
+    val maxDischargeCurrent: Double = 350.0,
+    val allowToDischarge: Boolean,
+    val nrOfModulesBlockingDischarge: Int,
+)
+
+data class ControlParams(
+    val chargeParams: ChargeParams,
+    val dischargeParams: DischargeParams,
+)
 
 data class DbusService(
     val service: String,
